976fde07486401331dc0a71c0ddf30b57b5eac33
ystsbry <jmachang368@gmail.com>
2025-12-05 19:43:36 +0900

    feat: レイアウト系Utilの実装

---
+++ hp-rendering-engine/src/parts/_shared/layout/applyLayoutConfig.ts
@@ -0,0 +1,54 @@
+import { px, Styles } from "../styleConfigApplier"
+import { assignInlineVars } from '@vanilla-extract/dynamic'
+import { Contract } from "./contract"
+import { ResponsiveCssVars } from "../responsive"
+
+export type LayoutConfigApplier = (
+  config: Contract.ResponsiveLayout,
+  cssVars: ResponsiveCssVars<Contract.Layout>
+) => Styles
+
+export const applyLayoutConfig: LayoutConfigApplier = (config, cssVars) => {
+  const base = config.base
+  const sm = config.sm
+  const md = config.md
+  const lg = config.lg
+
+  return assignInlineVars({
+    // base
+    [cssVars.base.size.width]: px(base.size.width),
+    [cssVars.base.size.height]: px(base.size.height),
+
+    [cssVars.base.position.top]: px(base.position.top),
+    [cssVars.base.position.left]: px(base.position.left),
+    [cssVars.base.position.right]: px(base.position.right),
+    [cssVars.base.position.bottom]: px(base.position.bottom),
+
+    // sm
+    [cssVars.sm.size.width]: px(sm?.size.width),
+    [cssVars.sm.size.height]: px(sm?.size.height),
+
+    [cssVars.sm.position.top]: px(sm?.position.top),
+    [cssVars.sm.position.left]: px(sm?.position.left),
+    [cssVars.sm.position.right]: px(sm?.position.right),
+    [cssVars.sm.position.bottom]: px(sm?.position.bottom),
+
+    // md
+    [cssVars.md.size.width]: px(md?.size.width),
+    [cssVars.md.size.height]: px(md?.size.height),
+
+    [cssVars.md.position.top]: px(md?.position.top),
+    [cssVars.md.position.left]: px(md?.position.left),
+    [cssVars.md.position.right]: px(md?.position.right),
+    [cssVars.md.position.bottom]: px(md?.position.bottom),
+
+    // lg
+    [cssVars.lg.size.width]: px(lg?.size.width),
+    [cssVars.lg.size.height]: px(lg?.size.height),
+
+    [cssVars.lg.position.top]: px(lg?.position.top),
+    [cssVars.lg.position.left]: px(lg?.position.left),
+    [cssVars.lg.position.right]: px(lg?.position.right),
+    [cssVars.lg.position.bottom]: px(lg?.position.bottom),
+  })
+}
---
+++ hp-rendering-engine/src/parts/_shared/layout/configure.ts
@@ -0,0 +1,11 @@
+import type { Contract } from './contract'
+
+export const defaultConfig: Contract.ResponsiveLayout = {
+  base: {
+    size: {
+      width: 0,
+      height: 0,
+    },
+    position: { top: 0, left: 0 },
+  },
+}
