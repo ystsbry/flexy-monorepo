277bf17dddc35b045b4e7eebfada75a8f517cc9e
ystsbry <jmachang368@gmail.com>
2025-12-06 21:59:14 +0900

    refactor(hp-rendering-engine): wrapIfの切り出し

---
+++ hp-rendering-engine/src/parts/_shared/conditionalWrap.ts
@@ -0,0 +1,5 @@
+import { JSX } from "solid-js/jsx-runtime"
+
+export const wrapIf =
+  (cond: boolean, wrapper: (children: JSX.Element) => JSX.Element) => (children: JSX.Element) =>
+    cond ? wrapper(children) : children
